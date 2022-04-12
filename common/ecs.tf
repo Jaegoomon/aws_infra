662fb5d62be15c2242d6c0dfab797407e2bba89d
Jaegoomon <jaegoo1199@gmail.com>
2022-04-12 10:20:21 +0900

    feat: [ecs-module] autoscale 보안그룹
    HTTP 연결 추가

--- common/ecs.tf
+++ common/ecs.tf
@@ -9,9 +9,41 @@ module ecs {
   key_name          = module.vpc.key_name
 
   subnet_ids    = module.vpc.private_subnet_ids
-  autoscale_sg  = [module.vpc.ssh_sg_id]
+  autoscale_sg  = [module.vpc.ssh_sg_id, aws_security_group.autoscale.id]
   
   autoscale_min     = 1
   autoscale_max     = 10
   autoscale_desired = 2
+}
+
+# Security group
+resource "aws_security_group" "autoscale" {
+  name        = "autoscale"
+  description = "Allow autoscale group instances traffix"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "autoscale security sg"
+  }
 }
